a2ac3940e712e9800195080a97630734d82671c6
Aliaic <592407012@qq.com>
2018-03-29 10:25:40 +0800

    test

---
+++ src/main/java/gradle/sample/helloGradle.java
@@ -0,0 +1,16 @@
+package gradle.sample;
+
+/**
+ * Created by yuchao.liang on 2018/3/7.
+ */
+import org.testng.annotations.*;
+
+public class helloGradle {
+    public void test(){
+        System.out.print("test01");
+    }
+//    public static void main(String[] args)
+//    {
+//        System.out.print("hello Gradle");
+//    }
+}
